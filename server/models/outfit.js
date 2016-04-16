1a4caec656f5c999b18e408dc3e3ccc1c18ec024
goodbedford <bedford@goodbedford.com>
2016-04-16 10:51:08 -0700

    added new users field to outfit schema

--- server/models/outfit.js
+++ server/models/outfit.js
@@ -1,10 +1,12 @@
 var mongoose    = require("mongoose");
 var Schema      = mongoose.Schema;
+var User        = require("./user.js");
 
 var OutfitSchema = new Schema({
     imgUrl:{type: String, required: true, lowercase: true},
     author: {type: String, required: true, lowercase: true},
     type: {type: String, required: true, lowercase: true},
+    users: [{type: Schema.Types.ObjectId, ref: "User"}],
     timestamps: {type: Date, default: Date.now}
 });
 
